8047a0200d22bcfc26af1f529725cc4754725244
Paolo <paolo.pertino@mail.polimi.it>
2021-10-27 17:58:14 +0000

    a

--- Esercitazione 5/Esercizio 2/src/Gabbia.java
+++ Esercitazione 5/Esercizio 2/src/Gabbia.java
@@ -1,9 +1,9 @@
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Gabbia {
     private CreatoreDiAnimali factory;
-    private List<Animale>  animali;
+    private List<Animale> animali;
 
     public Gabbia(CreatoreDiAnimali creatoreDiAnimali) {
         this.factory = creatoreDiAnimali;
